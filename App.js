e32b4e4cb3bc4559a752f0316719768d6a209f55
Manikana <maniking22000@gmail.com>
2024-08-07 18:55:46 +0530

    Used JSX in react application

--- App.js
+++ App.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading= React.createElement("h1",{},[React.createElement("div",{id: "child"},"Hello world"),React.createElement("div",{},"what's app")]);
+
+const jsxHeading=<h1>Hello JSX what's up</h1>
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+// root.render(jsxHeading);
